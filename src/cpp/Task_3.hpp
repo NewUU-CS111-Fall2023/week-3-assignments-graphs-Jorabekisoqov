c3f6db8329b137ca25a65d6f3b5792171b06c9df
Jorabekisoqov <127713082+Jorabekisoqov@users.noreply.github.com>
2023-10-31 19:40:36 +0500

    Update Task_3.hpp

--- src/cpp/Task_3.hpp
+++ src/cpp/Task_3.hpp
@@ -1,1 +1,38 @@
 
+int main(){
+    int count=0;
+    int a,b;
+    cin>>a>>b;
+    vector<int> vec;
+    vec.push_back(b);
+    while(b>0){
+        if(b%2==0){
+            b=b/2;
+            count++;
+            vec.push_back(b);
+
+            if(a==b){
+                break;
+            }
+        }
+        else if(b%10==1){
+            b=(b-1)/10;
+            count++;
+            vec.push_back(b);
+            if(a==b){
+                break;
+            }
+        }
+    }
+    sort(vec.begin(),vec.end());
+    if(a==b){
+        cout<<"Yes\n";
+        cout<<count+1<<endl;
+        for(int &i: vec){
+            cout<<i<<" ";
+        }
+    }
+    else{
+        cout<<"No";
+    }
+}
